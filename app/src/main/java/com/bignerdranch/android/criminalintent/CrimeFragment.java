b60692ca99ff476ed6999540454822ce791bfae5
John Smith <nilpoint@hotmail.com>
2016-10-03 20:54:04 +0800

    Formatting the date

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -51,8 +52,9 @@ public class CrimeFragment extends Fragment {
       }
     });
 
+    String dateText = DateFormat.format("E, d MMMM, yyyy", mCrime.getDate()).toString();
     mDateButton = (Button) v.findViewById(R.id.crime_date);
-    mDateButton.setText(mCrime.getDate().toString());
+    mDateButton.setText(dateText);
     mDateButton.setEnabled(false);
 
     mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
